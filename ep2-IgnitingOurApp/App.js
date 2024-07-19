b63c0599ecaa1c059b5695897e8c54b4dbf79af4
verma-shu6ham <verma.shu6ham@gmail.com>
2024-07-19 15:12:07 +0530

    ep2

---
+++ ep2-IgnitingOurApp/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" }, [
+        React.createElement("h1", {}, "I am a h1 tag"),
+        React.createElement("h2", {}, "I am a h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I am a h1 tag"),
+        React.createElement("h2", {}, "I am a h2 tag"),
+    ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
